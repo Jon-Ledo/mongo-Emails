8587d126c49fe685a205f3722e421c7f5e44c05b
JonLedo <jonledo.code@gmail.com>
2023-02-11 23:25:17 -0500

    added in code using send Grid instead of node mailer. Changed name of older function for reference (not in use)

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer')
 
-const sendEmail = async (req, res) => {
+const sendEmailEthereal = async (req, res) => {
   let testAccount = await nodemailer.createTestAccount()
 
   const transporter = nodemailer.createTransport({
@@ -23,6 +23,25 @@ const sendEmail = async (req, res) => {
 
   console.log(info)
   res.status(200).json(info)
+}
+
+const sendEmail = async (req, res) => {
+  // using Twilio SendGrid's v3 Node.js Library
+  // https://github.com/sendgrid/sendgrid-nodejs
+  // javascript
+  const sgMail = require('@sendgrid/mail')
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+  const msg = {
+    to: process.env.EMAIL_SEND, // Change to your recipient
+    from: process.env.EMAIL, // Change to your verified sender
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  }
+
+  const info = await sgMail.send(msg)
+  // res.json(info)
 }
 
 module.exports = sendEmail
